50fd3a8e72615443ad5843b2abd450f3c6ba9b45
Alex <41344035+Dedal378@users.noreply.github.com>
2023-03-10 13:31:12 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import vue from '@vitejs/plugin-vue'
+
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+import Unocss from 'unocss/vite'
+import { presetUno, presetIcons } from 'unocss'
+
+export default defineConfig({
+  plugins: [
+    vue({ reactivityTransform: true }),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetIcons({
+          collections: {
+            custom: {
+              circle: '<svg viewBox="0 0 120 120"><circle cx="60" cy="60" r="50"></circle></svg>',
+              /*And then, you can use it on your html: <span class="i-custom:circle"></span>*/
+            },
+            carbon: () => import('@iconify-json/carbon/icons.json').then((i) => i.default),
+            mdi: () => import('@iconify-json/mdi/icons.json').then((i) => i.default),
+            logos: () => import('@iconify-json/logos/icons.json').then((i) => i.default),
+          },
+          extraProperties: {
+            display: 'inline-block',
+            'vertical-align': 'middle',
+          },
+        })
+      ],
+    })
+  ],
+  define: { 'process.env': {}},
+  resolve: {
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+  },
+  server: { port: 3000 },
+  // change name to your repo name " base: '/REPO/' "
+  base: '/vue3-template/',
+})
